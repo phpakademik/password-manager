4371d29b2bb740c4e869d0ac6bf21563a079927d
phpakademik <xurshidbekmbs@gmail.com>
2024-10-01 08:56:06 +0500

    first

---
+++ database/migrations/2024_09_30_161647_create_passwords_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->string('login');
+            $table->string('password');
+            $table->text('info');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('passwords');
+    }
+};
