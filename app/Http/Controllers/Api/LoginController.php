f819f7793ed689cab92b4196053fed20efe38d57
phpakademik <xurshidbekmbs@gmail.com>
2024-10-24 08:15:21 +0500

    nimadur

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return null;
+    }
+}
--- database/migrations/2024_09_30_161511_create_roles_table.php
+++ database/migrations/2024_09_30_161511_create_roles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('role');
             $table->timestamps();
         });
     }
