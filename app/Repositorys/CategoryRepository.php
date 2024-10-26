67816c6b17488968c1016dac38592ee77bcc0fbb
phpakademik <xurshidbekmbs@gmail.com>
2024-10-26 13:42:28 +0500

    bilmadim

--- app/Repositorys/CategoryRepository.php
+++ app/Repositorys/CategoryRepository.php
@@ -6,5 +6,21 @@ use App\Models\Category;
 
 class CategoryRepository
 {
-
+    public function all(){
+        return Category::paginate();
+    }
+    public function one($id){
+        return Category::where(['id'=>$id])->first();
+    }
+    public function create($data){
+        return Category::create($data);
+    }
+    public function update($id, $data){
+        $one = $this->one($id);
+        if($one){
+            return Category::where(['id'=>$id])->update($data);
+        }else{
+            return false;
+        }
+    }
 }
